72f4dcd31692d731d75513f1e432fd6c4b6c9ce4
Davy Sproule <dsproule@vt.edu>
2022-10-11 19:35:38 -0400

    implement login, logout, and register in addition to User Model

--- HANDICAP_CALCULATOR/settings.py
+++ HANDICAP_CALCULATOR/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'calculator.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- calculator/views.py
+++ calculator/views.py
@@ -1,8 +1,55 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 
 def index(request):
     # return HttpResponse("Handicap Calculator in Progress")
     return render(request, 'index.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, 'login.html', {"message": 'Invalid login credentials'})
+
+    else:
+        return render(request, 'login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm = request.POST['confirmation']
+
+        if password != confirm:
+            return render(request, 'register.html', {'message': 'Passwords do not match'})
+
+        try:
+            user= User.objects.create_user(username, email, password)
+            user.save()
+        except:
+            return render(request, 'register.html', {'message': 'Username already in use'})
+
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, 'register.html')
+
+# Form class for scores
